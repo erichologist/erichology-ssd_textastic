f7174d66beb42ff435315b94a7333a7382282a3f
erichologist <erhughes@ucsd.edu>
2026-02-22 08:53:31 -0800

    Delete codepen/responsive-table (2) (1) directory

--- codepen/responsive-table (2) (1)/dist/script.js
+++
@@ -1,5 +0,0 @@
-import styledJsx from "https://esm.sh/styled-jsx";
-import BabelPluginSyntaxJsx from "https://esm.sh/@babel/plugin-syntax-jsx";
-import BabelPluginSyntaxClassProperties from "https://esm.sh/@babel/plugin-syntax-class-properties";
-// Responsive table method using display:block and data attributes
-// Thanks to @leefroese for suggestion about data attributes
