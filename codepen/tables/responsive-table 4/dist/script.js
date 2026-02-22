9528af6a634193da33032c76fee20eb7654b7957
erichologist <erhughes@ucsd.edu>
2026-02-22 08:51:28 -0800

    Add files via upload

---
+++ codepen/tables/responsive-table 4/dist/script.js
@@ -0,0 +1,5 @@
+import styledJsx from "https://esm.sh/styled-jsx";
+import BabelPluginSyntaxJsx from "https://esm.sh/@babel/plugin-syntax-jsx";
+import BabelPluginSyntaxClassProperties from "https://esm.sh/@babel/plugin-syntax-class-properties";
+// Responsive table method using display:block and data attributes
+// Thanks to @leefroese for suggestion about data attributes
